3e52222b166dfa482ab2748711fca7507b58bd7c
Hugo.V <hugo.vialaneix@gmail.com>
2025-03-27 13:06:23 +0100

    INITIALISATION

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+
+
+ini_set('display_errors', 1);
+
+error_reporting(E_ALL);
+
+
+require('Model/pdo.php');
+
+
+   
+    
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des Étudiants</title>
+    <style>
+    </style>
